151eb248440ff340793e7818642cbbe65e4f90f8
Ananya Butwong <60281589+Ananya2503@users.noreply.github.com>
2021-09-03 22:49:33 +0700

    Add circular progress

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,18 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:foodandbody/theme.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  // const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +20,7 @@ class _HomeState extends State<Home> {
           backgroundColor: Color(0xFFF6F6F6),
           elevation: 0,
           title: Text("หน้าหลัก",
-              style: Theme.of(context)
-                  .textTheme
-                  .headline5!
+              style: AppTheme.themeData.textTheme.headline5!
                   .merge(TextStyle(color: AppTheme.themeData.primaryColor))),
           actions: [
             IconButton(
@@ -32,18 +34,72 @@ class _HomeState extends State<Home> {
         body: SafeArea(
           child: ListView(
             children: <Widget>[
+              //today's calories
               Container(
-                  height: 51,
-                  color: Colors.amber,
-                  child: Text(
-                    "แคลอรี่วันนี้",
-                    style: Theme.of(context).textTheme.bodyText1!.merge(
-                        TextStyle(color: AppTheme.themeData.primaryColor)),
+                  color: Color(0xFFF6F6F6),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
+                    child: Text(
+                      "แคลอรีวันนี้",
+                      style: AppTheme.themeData.textTheme.bodyText1!.merge(
+                        TextStyle(color: AppTheme.themeData.primaryColor),
+                      ),
+                    ),
                   )),
+
+              //calories progress
               Container(
                 height: 287,
-                color: Colors.blue,
-                child: Text("กินได้อีก"),
+                color: Color(0xFFF6F6F6),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+                  child: Card(
+                    color: AppTheme.themeData.primaryColor,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(102, 18, 95, 0),
+                            child: CircularPercentIndicator(
+                                radius: 183,
+                                lineWidth: 8,
+                                percent: getPercent(),
+                                animation: true,
+                                center: Center(
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding:
+                                            EdgeInsets.fromLTRB(0, 25, 0, 0),
+                                        child: Text(
+                                          "กินได้อีก",
+                                          style: AppTheme
+                                              .themeData.textTheme.subtitle1!
+                                              .merge(TextStyle(
+                                                  color: Colors.white)),
+                                        ),
+                                      ),
+                                      //Text Widget: Show remain calories
+                                      getRemainCal(),
+                                      Text(
+                                        "แคล",
+                                        style: AppTheme
+                                            .themeData.textTheme.subtitle1!
+                                            .merge(
+                                                TextStyle(color: Colors.white)),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                progressColor: Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ),
               Container(
                 height: 51,
@@ -68,5 +124,20 @@ class _HomeState extends State<Home> {
             ],
           ),
         ));
+  }
+
+  Widget getRemainCal() {
+    var remainCal = 618; //query (goalCalories - totalCalories)
+    return Text(
+      "${remainCal.toString()}",
+      style: AppTheme.themeData.textTheme.headline3!
+          .merge(TextStyle(color: Colors.white)),
+    );
+  }
+
+  double getPercent() {
+    var goalCal = 1800;
+    var totalCal = 1182;
+    return (totalCal/goalCal);
   }
 }
