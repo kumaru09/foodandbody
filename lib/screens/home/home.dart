dabf54d919fe7df7079f3129a46ecaed4dfe4644
Ananya Butwong <ananya.butwong@gmail.com>
2021-09-08 11:55:13 +0700

    Edit home import

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,7 +3,6 @@ import 'package:foodandbody/screens/home/circularCaIndicator.dart';
 import 'package:foodandbody/widget/buttomAppBar.dart';
 import 'package:foodandbody/widget/linearNutrientIndicator.dart';
 import 'package:foodandbody/widget/menuCard.dart';
-import 'package:percent_indicator/percent_indicator.dart';
 import 'package:foodandbody/app/bloc/app_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
