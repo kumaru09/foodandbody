7fb35bbc896a81ff685283bbfeba9e237c4f06a8
Ananya Butwong <ananya.butwong@gmail.com>
2021-10-28 21:13:14 +0700

    Add edit profile page layout

--- lib/screens/edit_profile/edit_profile.dart
+++ lib/screens/edit_profile/edit_profile.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:foodandbody/app/bloc/app_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodandbody/models/user.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditProfile extends StatelessWidget {
   const EditProfile({Key? key}) : super(key: key);
@@ -26,7 +32,7 @@ class EditProfile extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
-              print("confirm");
+              Navigator.pop(context);
             },
             icon: Icon(
               Icons.done,
@@ -35,6 +41,234 @@ class EditProfile extends StatelessWidget {
           )
         ],
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 21, bottom: 44),
+              width: MediaQuery.of(context).size.width,
+              constraints: BoxConstraints(minHeight: 50),
+              color: Theme.of(context).primaryColor,
+              alignment: Alignment.topCenter,
+              child: _EditProfileImage(),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 16, top: 26, right: 15),
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.topLeft,
+              child: _EditUsername(),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 16, top: 26, right: 15),
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.topLeft,
+              child: _EditEmail(),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 16, top: 26, right: 15),
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.topLeft,
+              child: _EditPassword(),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 16, top: 26, right: 15),
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.topLeft,
+              child: _EditGender(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _EditProfileImage extends StatefulWidget {
+  const _EditProfileImage({Key? key}) : super(key: key);
+
+  @override
+  __EditProfileImageState createState() => __EditProfileImageState();
+}
+
+class __EditProfileImageState extends State<_EditProfileImage> {
+  late Future<XFile?> _image;
+  ImagePicker _imagePicker = ImagePicker();
+
+  _imagFromGallery() async {
+    Future<XFile?> image =
+        _imagePicker.pickImage(source: ImageSource.gallery, imageQuality: 50);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  _imageFromCamera() async {
+    Future<XFile?> image =
+        _imagePicker.pickImage(source: ImageSource.camera, imageQuality: 50);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  String getInitialImage(BuildContext context) {
+    User _user = context.read<AppBloc>().state.user;
+    if (_user.info!.photoUrl != "")
+      return _user.info!.photoUrl.toString();
+    else if (_user.photoUrl != null)
+      return _user.photoUrl.toString();
+    else
+      return "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      Container(
+          child: CircleAvatar(
+        radius: 77,
+        backgroundColor: Colors.white,
+        child: CircleAvatar(
+          foregroundImage: getInitialImage(context) != ""
+              ? NetworkImage(
+                  context.read<AppBloc>().state.user.photoUrl.toString())
+              : Image.asset("assets/default_profile_image.png")
+                  .image,
+          backgroundColor: Colors.white,
+          radius: 74,
+        ),
+      )),
+      Positioned(
+          top: MediaQuery.of(context).size.height * 0.01,
+          left: MediaQuery.of(context).size.width * 0.255,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                primary: Theme.of(context).colorScheme.secondary),
+            child: Icon(
+              Icons.edit,
+              color: Colors.white,
+            ),
+            onPressed: () => _showSelectSheet(context),
+          ))
+    ]);
+  }
+
+  _showSelectSheet(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Wrap(
+            children: [
+              ListTile(
+                leading: Icon(Icons.photo_camera),
+                title: Text("กล้องถ่ายรูป"),
+                onTap: () {
+                  _imageFromCamera();
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.collections),
+                title: Text("แกลลอรี"),
+                onTap: () {
+                  _imagFromGallery();
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
+  }
+}
+
+class _EditUsername extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      initialValue: context.read<AppBloc>().state.user.info!.name,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          labelText: "ชื่อผู้ใช้งาน",
+          border: OutlineInputBorder(borderSide: BorderSide())),
+    );
+  }
+}
+
+class _EditEmail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      initialValue: context.read<AppBloc>().state.user.email,
+      textInputAction: TextInputAction.next,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          labelText: "อีเมล",
+          border: OutlineInputBorder(borderSide: BorderSide())),
+    );
+  }
+}
+
+class _EditPassword extends StatefulWidget {
+  @override
+  __EditPasswordState createState() => __EditPasswordState();
+}
+
+class __EditPasswordState extends State<_EditPassword> {
+  bool _isHidden = true;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        labelText: "รหัสผ่าน",
+        border: OutlineInputBorder(
+          borderSide: BorderSide(),
+        ),
+        suffixIcon: InkWell(
+          child: Icon(_isHidden ? Icons.visibility : Icons.visibility_off),
+          onTap: () {
+            setState(() {
+              _isHidden = !_isHidden;
+            });
+          },
+        ),
+      ),
+      obscureText: _isHidden,
+    );
+  }
+}
+
+class _EditGender extends StatefulWidget {
+  @override
+  __EditGenderState createState() => __EditGenderState();
+}
+
+class __EditGenderState extends State<_EditGender> {
+  @override
+  Widget build(BuildContext context) {
+    String? _gender = context.read<AppBloc>().state.user.info!.gender;
+
+    return DropdownButtonFormField(
+      value: _gender,
+      decoration: InputDecoration(
+        labelText: "เพศ",
+        border: OutlineInputBorder(borderSide: BorderSide()),
+      ),
+      items: [
+        DropdownMenuItem<String>(
+          child: Text("ชาย"),
+          value: "M",
+        ),
+        DropdownMenuItem<String>(
+          child: Text("หญิง"),
+          value: "F",
+        )
+      ],
+      onChanged: (String? gender) {
+        setState(() {
+          _gender = gender;
+        });
+      },
     );
   }
 }
