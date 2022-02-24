fec1cb521c93c54f5ec54bb904031533c077d273
Ausanee26 <60252895+Ausanee26@users.noreply.github.com>
2022-02-24 23:58:36 +0700

    Fix logout pop bug

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -2,12 +2,12 @@ import 'package:flutter/widgets.dart';
 import 'package:foodandbody/app/bloc/app_bloc.dart';
 import 'package:foodandbody/screens/initial_info/initial_info.dart';
 import 'package:foodandbody/screens/login/login.dart';
-import 'package:foodandbody/screens/main_screen/main_page.dart';
+import 'package:foodandbody/screens/main_screen/main_screen.dart';
 
 List<Page> onGenerateAppViewPages(AppStatus state, List<Page<dynamic>> pages) {
   switch (state) {
     case AppStatus.authenticated:
-      return [MainPage.page()];
+      return [MainScreen.page()];
     case AppStatus.initialize:
       return [InitialInfo.page()];
     case AppStatus.unauthenticated:
