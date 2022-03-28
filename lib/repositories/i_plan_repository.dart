694921e636c7fe4cfdf2539fda55188d45a672e3
Supharoek <ueki2542@gmail.com>
2022-03-28 21:42:53 +0700

    Fix eatNow and delete menu by volume

--- lib/repositories/i_plan_repository.dart
+++ lib/repositories/i_plan_repository.dart
@@ -2,11 +2,12 @@ import 'package:foodandbody/models/menu.dart';
 import 'package:foodandbody/models/menu_show.dart';
 
 abstract class IPlanRepository {
-  Future<void> addPlanMenu(String name, double volumn, bool isEat);
+  Future<void> addPlanMenu(
+      String name, double oldVolumn, double volumn, bool isEat);
 
   Future<void> getPlanById();
 
   Future<void> updatePlan(MenuShow menuDetail, double volumn, bool isEat);
 
-  Future<void> deletePlan(String name);
+  Future<void> deletePlan(String name, double volume);
 }
