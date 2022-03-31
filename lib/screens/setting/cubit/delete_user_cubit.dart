850cc8d2a698957996c78fcec20e153ad42be6ee
Supharoek <ueki2542@gmail.com>
2022-03-31 23:38:38 +0700

    Move deleteUser, fix getInfo

--- lib/screens/setting/cubit/delete_user_cubit.dart
+++ lib/screens/setting/cubit/delete_user_cubit.dart
@@ -1,19 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:foodandbody/models/password.dart';
-import 'package:foodandbody/repositories/user_repository.dart';
+import 'package:foodandbody/repositories/authen_repository.dart';
 import 'package:formz/formz.dart';
 
 part 'delete_user_state.dart';
 
 class DeleteUserCubit extends Cubit<DeleteUserState> {
-  DeleteUserCubit(this._userRepository) : super(const DeleteUserState());
+  DeleteUserCubit(this._authenRepository) : super(const DeleteUserState());
 
-  final UserRepository _userRepository;
+  final AuthenRepository _authenRepository;
 
   Future<void> deleteUser() async {
     try {
-      await _userRepository.deleteUser(state.password.value);
+      await _authenRepository.deleteUser(state.password.value);
       emit(state.copyWith(status: FormzStatus.submissionSuccess));
     } on DeleteUserFailure catch (e) {
       emit(state.copyWith(
